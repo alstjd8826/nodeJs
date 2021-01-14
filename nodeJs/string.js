4ca761f46fc368b6070146654156438c6420b11e
alstjd8826 <53422859+alstjd8826@users.noreply.github.com>
2021-01-14 19:18:37 +0900

    Add files via upload

---
+++ nodeJs/string.js
@@ -0,0 +1,2 @@
+console.log("1"+"1");
+console.log("Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem magni ullam, fugit veritatis ad suscipit aliquid, accusantium saepe sint architecto ducimus corrupti exercitationem consequatur omnis totam ipsum tenetur cumque? Ad!".length);
